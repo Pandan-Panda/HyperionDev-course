3288479f928ca4aca9e6eec6831b827b5b8c50eb
Pandan-Panda <153450921+Pandan-Panda@users.noreply.github.com>
2023-12-13 21:25:48 +0000

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,20 @@
+#hello_world
+#Task 1 Practical 1
+
+name  = input("What is your name?") #assigned a variable for user's name input (name is easiest to understand)
+
+print()  #using this allows there to be a space inbetween the sentences displayed on the console/terminal (makes reading easier)
+
+print("Hello " + name)     #this displays the user's name input in a short sentence/phrase
+
+print()
+
+age  = input("How old are you? ")     #assigned 'age' as a variable
+
+print()
+
+print("You are " + age + " years old." )  #this displays the user's age input in a small sentence
+
+print()  
+
+print("Hello World!")
